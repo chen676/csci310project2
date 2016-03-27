ad22b661cf0e4f89eebb21ff84d0356e703e67f9
Patrick Vanderwall <pvanderw@usc.edu>
2016-03-27 23:31:13 +0100

    fixed routes file

--- BudgetMe/app/Http/routes.php
+++ BudgetMe/app/Http/routes.php
@@ -17,21 +17,10 @@ Route::group(['middleware' => ['web']], function () {
         return view('login');
     });
 
-<<<<<<< HEAD
   	Route::post('/login', 'UserController@login');
   	Route::get('/dashboard', 'UserController@dashboard');
   	Route::get('/logout', 'UserController@logout');
 
-
-=======
-    Route::get('login', function () {
-        return View::make('login');
-    });
-
-    Route::get('dashboard', function () {
-        return View::make('dashboard');
-    });
->>>>>>> master
 });
 
 
