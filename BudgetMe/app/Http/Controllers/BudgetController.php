c89ba6a183fd35172ec1164bd0c5e3a36e3cf862
Harshul <hmulchan@usc.edu>
2016-03-30 05:47:23 +0100

    paul HElp

--- BudgetMe/app/Http/Controllers/BudgetController.php
+++ BudgetMe/app/Http/Controllers/BudgetController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Models\Budget;
 use DB;
 
 class BudgetController extends Controller
@@ -12,8 +13,15 @@ class BudgetController extends Controller
     public function getBudgets(Request $request){
 	if($request->ajax())
 	{
-    		return "ok";
-	}
+		$users = DB::select('select * from users');
+    		
+    	//echo ($request['data']);
+
+    	//return $users;
+
+    	echo json_encode($users);	
+    }
+
     }
 }
 
