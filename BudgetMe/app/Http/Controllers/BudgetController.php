1cabffd20e32f5fb491270cfbb49bac63545d8bd
Paul Wat <pwat@usc.edu>
2016-03-30 06:08:03 +0100

    fix

--- BudgetMe/app/Http/Controllers/BudgetController.php
+++ BudgetMe/app/Http/Controllers/BudgetController.php
@@ -7,21 +7,22 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\Budget;
 use DB;
+use Session;
 
 class BudgetController extends Controller
 {
     public function getBudgets(Request $request){
-	if($request->ajax())
-	{
-	   $user = Session::get('user');
-		//$users = DB::select('select * from users');
-    		
-    	//echo ($request['data']);
+	   if($request->ajax())
+	   {
+	      $user = Session::get('user');
+		   //$users = DB::select('select * from users');
+       		
+       	//echo ($request['data']);
 
-    	//return $users;
+       	//return $users;
 
-      return $user;
-    }
+         return "hi";
+       }
 
     }
 }
