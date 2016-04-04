023d21ef8240b8c150714247edeff5ce776c21bf
Patrick Vanderwall <pvanderw@usc.edu>
2016-04-04 01:39:05 +0100

    Login tests  with correct/incorrect info

--- BudgetMe/tests/UserControllerTest.php
+++ BudgetMe/tests/UserControllerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class UserControllerTest extends TestCase
 {
-    public function testLogin()
+    public function testLoginWithCorrectInfo()
     {
     	$this
     		->visit('/')
@@ -14,6 +14,16 @@ class UserControllerTest extends TestCase
     		->type('password', 'password')
     		->press('Login')
     		->seePageIs('/dashboard');
+    }
+
+    public function testLoginWithIncorrectInfo()
+    {
+    	$this
+    		->visit('/')
+    		->type('wrong_email', 'email')
+    		->type('wrong_password', 'password')
+    		->press('Login')
+    		->see('Email or password is incorrect. Please try again.');
     }
 
     public function testLogout()
