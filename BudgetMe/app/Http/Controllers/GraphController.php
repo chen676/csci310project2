0ef38d182ab6f0b785b40bd06ed55e220b8e179f
Matt Wenner <pwat@usc.edu>
2016-04-13 06:04:37 +0100

    Matt and Harshul almost done with graphing assets and liabilities

--- BudgetMe/app/Http/Controllers/GraphController.php
+++ BudgetMe/app/Http/Controllers/GraphController.php
@@ -15,13 +15,24 @@ use DB;
 use App\Library\TransactionManager;
 
 class GraphController extends Controller{
+
+	/*
+		Parameters: Request
+
+		Description: Populates the default graph with assets and liabilities
+
+		Returns:
+
+		Created By: Matt and Harshul
+	*/
 	public function populateGraph(Request $request){
 
 		if($request->ajax()){
+			
 			$user = Session::get('user');
 			$user_id = $user -> id;
-			$startingDate = $request['starting_date'];
-			$endingDate = $request['ending_date'];
+			$startDate = $request['starting_date'];
+			$endDate = $request['ending_date'];
 			$accounts = DB::select('select * from accounts where user_id = :user_id' ,  ['user_id' => $user_id]);
 			$valid_account_ids = array();
 			foreach($accounts as $account){
@@ -42,15 +53,32 @@ class GraphController extends Controller{
 			$transactions = $transactionManager -> sortTransactionsByDates($transactions);
 			$transactions = array_reverse($transactions);
 
+			$prevBalanceAssets = 0;
+			$prevBalanceLiabilities = 0;
+			$transactionBeforeStartExistsAssets = false;
+			$transactionBeforeStartExistsLiabilities = false;
+
+			if(empty($transactions)){
+				return array();
+			}
+
 			$assetsData = array();
 			$liabilitiesData = array();
 			foreach($transactions as $t){
 				/*if date is before startDate, skip this transaction*/
-				if($transactionManager->rawDateCompare($t->date, $startingDate) > 0){
+				if($transactionManager->rawDateCompare($t->date, $startDate) > 0){
+					if($t->amount > 0){
+						$prevBalanceAssets += $t->amount;
+						$transactionBeforeStartExistsAssets = true;
+					}
+					else{
+						$prevBalanceAssets += -1 * $t->amount;
+						$transactionBeforeStartExistsLiabilities = false;
+					}
 					continue;
 				}
 				/*if date is after endDate, skip this transaction*/
-				if($transactionManager->rawDateCompare($t->date, $endingDate) < 0){
+				if($transactionManager->rawDateCompare($t->date, $endDate) < 0){
 					continue;
 				}
 				/*amount is greater than zero, therefore an asset*/
@@ -68,9 +96,53 @@ class GraphController extends Controller{
 					$liabilitiesData[$t->date] += -1 * $t->date;
 				}
 			}
-			return 5;
+			//cumulate each data point for assets and liabilities
+			$netAssets = $prevBalanceAssets;
+			$netLiabilities = $prevBalanceLiabilities;
+			foreach($assetsData as &$data){
+				$netAssets += $data;
+				$data = $netAssets;
+			}
+
+			foreach($liabilitiesData as &$data){
+				$netLiabilities += $data;
+				$data = $netLiabilities;
+			}
+
+			$paddingLeftAssets = array();
+			$paddingRightAssets = array();
+
+			$paddingLeftLiabilities = array();
+			$paddingRightLiabilities = array();
+
+			if($transactionBeforeStartExistsAssets){
+				if(!array_key_exists($startDate, $assetsData)){
+					$paddingLeftAssets[$startDate] = $prevBalanceAssets;
+				}
+			}
+			if(!array_key_exists($endDate, $assetsData)){
+				$paddingRightAssets[$endDate] = $netAssets;
+			}
+
+			$fullAssetsData = array_merge($paddingLeftAssets, $assetsData, $paddingRightAssets);
+
+			if($transactionBeforeStartExistsLiabilities){
+				if(!array_key_exists($startDate, $liabilitiesData)){
+					$paddingLeftLiabilities[$startDate] = $prevBalanceLiabilities;
+				}
+			}
+			if(!array_key_exists($endDate, $liabilitiesData)){
+				$paddingRightLiabilities[$endDate] = $netLiabilities;
+			}
+
+			$fullLiabilitiesData = array_merge($paddingLeftLiabilities, $liabilitiesData, $paddingRightLiabilities);
+			$totalData = array();
+			array_push($totalData, $fullAssetsData);
+			array_push($totalData, $fullLiabilitiesData);
+			return $totalData;
 
 			
+
 
 		}
 	}
@@ -92,10 +164,19 @@ class GraphController extends Controller{
 
 	    $graphData = array();
 	    $cDate = $startDate;
+	    $prevBalance = 0;
+	    $transactionBeforeStartExists = false;
+
+	    if(empty($tset))
+	    	return array();
+	    
 	    foreach($tset as $t){
 	    	//if cDate is before startDate, skip 
-	    	if($tm->rawDateCompare($t['date'], $startDate) > 0)
+	    	if($tm->rawDateCompare($t['date'], $startDate) > 0) {
+	    		$prevBalance += $t['amount'];
+	    		$transactionBeforeStartExists = true;
 	    		continue;
+	    	}
 	    	//if date is after endDate, skip
 	    	if($tm->rawDateCompare($t['date'], $endDate) < 0)
 	    		continue;
@@ -105,10 +186,67 @@ class GraphController extends Controller{
 	    	$graphData[$t['date']] += $t['amount'];
 	    }
 	    
-	    var_dump($graphData);
+	    //cumulate each data point
+	    $net = $prevBalance;
+	    foreach($graphData as &$g){
+	    	$net += $g;
+	    	$g = $net;
+	    }
+	    //all data points betwen sDate and earliest transaction are $prevBalance
+	    $paddingLeft = array();
+	    $paddingRight = array();
+
+	    if($transactionBeforeStartExists){
+		    if(!array_key_exists($startDate, $graphData))
+		    	$paddingLeft[$startDate] = $prevBalance;
+		}
+	    if(!array_key_exists($endDate, $graphData))
+	    	$paddingRight[$endDate] = $net;	    
+	  
+	    //all points between latest transaction and fDate are $net
+	    return array_merge($paddingLeft, $graphData, $paddingRight);
 	}
 
-	public function test(){
-		$this->getGraphDataForAnAccount("03/27/2014", "03/30/2016", 13);
+	/*
+		Parameters: Request
+
+		Description: get aggregate accounts to graph 
+
+		Returns: associative array of accounts and balances for dates within the start and end
+
+		Created By: Paul and Rebecca
+	*/
+	public function getAccountSetForGraph(Request $request){
+ 		if($request->ajax()){
+			if($_POST['length'] == 0){
+			  Session::put('checkedAccountsForGraph', array());
+			  return;
+			}
+
+			$sDate = $_POST['sDate'];
+			$eDate = $_POST['eDate'];
+			if($sDate == "")
+				$sDate = "01/01/1990";
+			if($eDate == "")
+				$eDate = "04/12/3000";
+
+			$accountNames = $_POST['accountSet'];
+			//need to create an array based on id, not name
+			$user = Session::get('user');
+			
+			$graphData = array();
+
+			foreach($accountNames as $name){
+			  $account = Account::where('name', '=', $name)
+			  ->where('user_id', '=', $user->id)
+			  ->get()->first();
+			  $aid = $account->id;
+			  $data = $this->getGraphDataForAnAccount($sDate, $eDate, $aid);
+			  $graphData[$name] = $data;
+			}
+
+			Session::put('checkedAccountsForGraph', $accountNames);
+			return $graphData;
+      	}		
 	}
 }
